2b30ddbc9f8bd5d5a543eb42593d5e98c3706092
g0mb4 <gomba007@gmail.com>
2022-03-01 15:42:15 +0100

    Simplify main().

--- turtle_catcher/src/game_master_node.cpp
+++ turtle_catcher/src/game_master_node.cpp
@@ -4,8 +4,7 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<GameMaster>();
-    rclcpp::spin(node);
+    rclcpp::spin(std::make_shared<GameMaster>());
 
     rclcpp::shutdown();
     return 0;
--- turtle_catcher/src/turtle1_controller_node.cpp
+++ turtle_catcher/src/turtle1_controller_node.cpp
@@ -4,8 +4,7 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<Turtle1Controller>();
-    rclcpp::spin(node);
+    rclcpp::spin(std::make_shared<Turtle1Controller>());
 
     rclcpp::shutdown();
     return 0;
