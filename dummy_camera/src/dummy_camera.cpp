058273810c7add91efab4626ac51bb1602a9ae77
g0mb4 <gomba007@gmail.com>
2022-03-01 15:41:12 +0100

    Update dummy_camera code.

--- dummy_camera/src/dummy_camera.cpp
+++ dummy_camera/src/dummy_camera.cpp
@@ -1,12 +1,13 @@
-#include <rclcpp/rclcpp.hpp>
-#include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
-class DummyCameraNode: public rclcpp::Node 
-{
+class DummyCameraNode : public rclcpp::Node {
 public:
-    DummyCameraNode() : rclcpp::Node("dummy_camera"){
+    DummyCameraNode()
+        : rclcpp::Node("dummy_camera")
+    {
         declare_parameter<std::vector<std::string>>("files", {});
         declare_parameter<int64_t>("fps", 30);
 
@@ -14,10 +15,10 @@ public:
         int64_t frequency = (int64_t)((1000.0) / (double)fps);
 
         auto files = get_parameter("files").as_string_array();
-        for(auto & file: files){
+        for (auto& file : files) {
             cv::Mat image = cv::imread(file);
 
-            if(image.empty()){
+            if (image.empty()) {
                 RCLCPP_WARN(get_logger(), "unable to load file: %s", file.c_str());
             } else {
                 m_images.emplace_back(image);
@@ -25,45 +26,39 @@ public:
             }
         }
 
-        if(m_images.size() == 0){
-            RCLCPP_ERROR(get_logger(), "no files to show");
-            m_error = true;
-            return;
+        if (m_images.size() == 0) {
+            RCLCPP_FATAL(get_logger(), "no files to show");
+            exit(1);
         }
 
-        m_publisher = create_publisher<sensor_msgs::msg::Image>("dummy_camera/image", 1);
-        m_timer = create_wall_timer(std::chrono::milliseconds(frequency), 
-                                    std::bind(&DummyCameraNode::publish_image, this));
+        m_publisher = create_publisher<sensor_msgs::msg::Image>("/image", 1);
+        m_timer = create_wall_timer(std::chrono::milliseconds(frequency),
+            std::bind(&DummyCameraNode::publish_image, this));
 
         RCLCPP_INFO(get_logger(), "started");
     }
 
-    bool has_error() const { return m_error; };
 private:
-    void publish_image(){
-        auto image = cv_bridge::CvImage(std_msgs::msg::Header(), 
-                     "bgr8", m_images[m_counter]).toImageMsg();
+    void publish_image()
+    {
+        auto image = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", m_images[m_counter]).toImageMsg();
         image->header.frame_id = "camera";
-        image->header.stamp = rclcpp::Clock().now();
+        image->header.stamp = now();
         m_publisher->publish(*image);
-        m_counter = m_counter + 1 < m_images.size() ? m_counter + 1 : 0;
+        m_counter = ++m_counter % m_images.size();
     }
 
-    bool m_error { false };
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr m_publisher;
     rclcpp::TimerBase::SharedPtr m_timer;
     std::vector<cv::Mat> m_images;
     uint32_t m_counter { 0 };
 };
 
-int main(int argc, char ** argv){
+int main(int argc, char** argv)
+{
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<DummyCameraNode>();
-
-    if(node->has_error() == false){
-        rclcpp::spin(node);
-    }
+    rclcpp::spin(std::make_shared<DummyCameraNode>());
 
     rclcpp::shutdown();
     return 0;
