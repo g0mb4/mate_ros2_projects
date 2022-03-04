f6197e4910da04bacb1ccc56e1e7ef86cd981061
g0mb4 <gomba007@gmail.com>
2022-03-04 12:52:26 +0100

    Add rviz config.

--- gazebo_demo/launch/start.launch.py
+++ gazebo_demo/launch/start.launch.py
@@ -11,11 +11,29 @@ def generate_launch_description():
 
     gazeboDemoSharedDir = get_package_share_directory('gazebo_demo')
     worldFile = os.path.join(gazeboDemoSharedDir, 'world/diff_drive_robot.world')
+    rvizConfigFile = os.path.join(get_package_share_directory('gazebo_demo'), 'rviz/gazebo_demo.rviz')
 
     ld.add_action(
         ExecuteProcess(
             cmd=['gazebo', '--verbose', worldFile],
             output='screen'
+        )
+    )
+
+    ld.add_action(
+        Node(
+            package = "tf2_ros", 
+            executable = "static_transform_publisher",
+            arguments = ["0.5", "0", "0.25", "0", "0", "0", "chassis", "lidar_link"]
+        )
+    )
+
+    ld.add_action(
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            output='screen',
+            arguments=['-d', rvizConfigFile]
         )
     )
 
